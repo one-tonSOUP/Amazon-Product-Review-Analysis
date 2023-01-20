a354fa1b5f4b0d050cd4a7dbddcc3b5849e02f51
one-tonSOUP <bhimandey@gmail.com>
2023-01-20 20:24:27 +0530

    Divided into methods, saved the data into CSV file(Stage 3)

--- Scrapper.py
+++ Scrapper.py
@@ -1,59 +1,116 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+import os
 
-url = 'https://p-nt-www-amazon-in-kalias.amazon.in/New-Apple-iPhone-12-128GB/dp/B08L5VJWCV?th=1'
+def get_response(url):
+    response = requests.get(url)
+    return BeautifulSoup(response.text, 'html.parser')
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+def get_rating(review):
+    rating = review.find('span', class_ = "a-icon-alt").text
+    rating = rating.split("out")[0].strip()
+    rating = float(rating)
+    return rating
 
-reviews = []
-for review in soup.find_all('div', {'class': 'review'}):
-    name = review.find('span', class_ = "a-profile-name").text
-    star = review.find('span', class_ = "a-icon-alt").text
-    date = review.find('span', class_ = "a-size-base a-color-secondary review-date").text
-    comment = review.find('a', class_ = "a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold").text.strip()
-    body = review.find('div', class_ = "a-expander-content reviewText review-text-content a-expander-partial-collapse-content").text.strip()
-    img_links = []
-    vote = review.find('span', class_ = "a-size-base a-color-tertiary cr-vote-text")
+def get_star(rating):
+    rating = int(rating)
+    star = "⭐" * rating
+    return star
+
+def get_variant(product, review):
     variant = review.find('span', {'data-hook': 'format-strip-linkless'})
-
-    product_name = "Not Available"
+    product_name = product
     size = "Not Available"
     colour = "Not Available"
     # Filtering the 'None' type data..
-    if vote != None:
-      vote = vote.text
     if variant != None:
-      variant = variant.text
-      temp, product_name = str(variant).split("Pattern name: ")
-      temp, size = temp.split("Size: ")
-      temp, colour = temp.split("Colour: ")
-    if review.find('div', class_ = "review-image-tile-section") == None:
-      img_links.append("Not Available")
+        variant = variant.text
+        temp, product_name = str(variant).split("Pattern name: ")
+        temp, size = temp.split("Size: ")
+        temp, colour = temp.split("Colour: ")
+    elif variant == None:
+        product_name, size, colour = product.split('(')
+        product_name = product_name.strip() + " (Generated_from_Title)"
+        size = size.split(')')[0].strip() + " (Generated_from_Title)"
+        colour = colour.split(')')[-1].strip() + " (Generated_from_Title)"
+    return product_name, size, colour
+
+def get_vote(review):
+    vote = review.find('span', class_ = "a-size-base a-color-tertiary cr-vote-text")
+    if vote != None:
+        vote = vote.text
     else:
-      raw_img_links = review.find_all('img', alt = "Customer image")
-      for individual_links in raw_img_links:
+        vote = "None"
+    return vote
+
+def get_review_images(review):
+    img_links = []
+    raw_img_links = []
+    if review.find('div', class_ = "review-image-tile-section") == None:
+        img_links.append("Not Available")
+    else:
+        raw_img_links = review.find_all('img', alt = "Customer image")
+    for individual_links in raw_img_links:
         media_link = individual_links.get('data-src')
         if media_link != None:
-          img_links.append(media_link)
+            img_links.append(media_link)
+    return ', '.join(img_links)
 
-    # Printing the details..
-    print("\n\n\n\nNAME      :   ", name)
-    print("PRODUCT   :   ", product_name)
-    print("COLOUR    :   ", colour)
-    print("SIZE      :   ", size)
-    print("STAR      :   ", star)
-    print("DATE      :   ", date.encode("utf-8"))
-    print("COMMENT   :   ", comment.encode("utf-8"))
-    print("BODY      :   ", body.encode("utf-8"))
-    print("IMAGES    :   ", img_links)
-    print("VOTE      :   ", vote)
+def scrape(url):
+    soup = get_response(url)
+    product = soup.find('span', class_ = "a-size-large product-title-word-break").text.strip()
+    print("\t\t\tP R O D U C T     :   ", product)
+    fetched_reviews = []
+    for review in soup.find_all('div', {'class': 'review'}):
+        customer_name = review.find('span', class_ = "a-profile-name").text
+        rating = get_rating(review)
+        date = review.find('span', class_ = "a-size-base a-color-secondary review-date").text
+        comment = review.find('a', class_ = "a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold").text.strip()
+        body = review.find('div', class_ = "a-expander-content reviewText review-text-content a-expander-partial-collapse-content").text.strip()
+        product_name, size, colour = get_variant(product, review)
+        vote = get_vote(review)
+        img_links = get_review_images(review)
 
-#print(type(reviews))
-#print(len(reviews))
+        #['Customer Name', 'Product Name', 'Size', 'Colour', 'Rating(Out of 5)', 'Rating', 'Date of Review', 'Comment', 'Review', 'Images attatched by Customer', 'Votes on the Review']
+        fetched_reviews.append([customer_name, product_name, size, colour, rating, get_star(rating), date, comment, body, img_links, vote])
 
-#for key, value in reviews:
-#    print("Key: ", key)
-#    print("Value : ", value.encode('utf-8'))
+        # Printing the details..
+        print("\n\n\n\nNAME      :   ", customer_name)
+        print("PRODUCT   :   ", product_name)
+        print("COLOUR    :   ", colour)
+        print("SIZE      :   ", size)
+        print("RATING    :   ", rating)
+        print("DATE      :   ", date.encode("utf-8"))       # '.encode("utf-8")' is required only in Visual Studio Code or CMD, not required if used in Colab..
+        print("COMMENT   :   ", comment.encode("utf-8"))    # '.encode("utf-8")' is required only in Visual Studio Code or CMD, not required if used in Colab..
+        print("BODY      :   ", body.encode("utf-8"))       # '.encode("utf-8")' is required only in Visual Studio Code or CMD, not required if used in Colab..
+        print("IMAGES    :   ", img_links)
+        print("VOTE      :   ", vote)
 
-#print(response.content)
+    # Save to CSV..
+    save_to_csv(product, fetched_reviews)
+    
+def save_to_csv(product, fetched_reviews):
+    page_visit_details = datetime.now().strftime("%B %d %Y, %H-%M-%S")
+    file_path = product + '(Product Page, Visited on ' + str(page_visit_details) + ').csv'
+    # write the information to a CSV file
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Customer Name', 'Product Name', 'Size', 'Colour', 'Rating(Out of 5)', 'Rating', 'Date of Review', 'Comment', 'Review', 'Images attatched by Customer', 'Votes on the Review'])
+        csv_writer.writerows(fetched_reviews)
+    
+    # Get the absolute path of the file
+    absolute_path = os.path.abspath(file_path)
+    # Print the absolute path
+    print("\n\n___________________________________________________________________________________________________________________________________")
+    print("-----------------------------------------------------------------------------------------------------------------------------------\n\n")
+    print(f'The absolute path of the CSV file is: {absolute_path}\nVisited on {page_visit_details}')
+
+def save_to_json():
+    pass
+
+url = 'https://p-nt-www-amazon-in-kalias.amazon.in/New-Apple-iPhone-12-128GB/dp/B08L5VJWCV?th=1'
+
+if __name__ == "__main__":
+    scrape(url)
--- test.py
+++ test.py
@@ -1,59 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+# Extract rating from the string..
 
-url = 'https://p-nt-www-amazon-in-kalias.amazon.in/New-Apple-iPhone-12-128GB/dp/B08L5VJWCV?th=1'
+'''star = "5.0 out of 5 stars"
+rating = star.split("out")[0].strip()
+print(rating)
+print(type(rating))
+rating = float(rating)
+print(rating)
+print(type(rating))
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+print("\{U+2730}")'''
 
-reviews = []
-for review in soup.find_all('div', {'class': 'review'}):
-    name = review.find('span', class_ = "a-profile-name").text
-    star = review.find('span', class_ = "a-icon-alt").text
-    date = review.find('span', class_ = "a-size-base a-color-secondary review-date").text
-    comment = review.find('a', class_ = "a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold").text.strip()
-    body = review.find('div', class_ = "a-expander-content reviewText review-text-content a-expander-partial-collapse-content").text.strip()
-    img_links = []
-    vote = review.find('span', class_ = "a-size-base a-color-tertiary cr-vote-text")
-    variant = review.find('span', {'data-hook': 'format-strip-linkless'})
-
-    product_name = "Not Available"
-    size = "Not Available"
-    colour = "Not Available"
+#def get_variant(product, review):
+#    variant = review.find('span', {'data-hook': 'format-strip-linkless'})
+#    product_name = product
+#    size = "Not Available"
+#    colour = "Not Available"
     # Filtering the 'None' type data..
-    if vote != None:
-      vote = vote.text
-    if variant != None:
-      variant = variant.text
-      temp, product_name = str(variant).split("Pattern name: ")
-      temp, size = temp.split("Size: ")
-      temp, colour = temp.split("Colour: ")
-    if review.find('div', class_ = "review-image-tile-section") == None:
-      img_links.append("Not Available")
-    else:
-      raw_img_links = review.find_all('img', alt = "Customer image")
-      for individual_links in raw_img_links:
-        media_link = individual_links.get('data-src')
-        if media_link != None:
-          img_links.append(media_link)
 
-    # Printing the details..
-    print("\n\n\n\nNAME      :   ", name)
-    print("PRODUCT   :   ", product_name)
-    print("COLOUR    :   ", colour)
-    print("SIZE      :   ", size)
-    print("STAR      :   ", star)
-    print("DATE      :   ", date)
-    print("COMMENT   :   ", comment)
-    print("BODY      :   ", body)
-    print("IMAGES    :   ", img_links)
-    print("VOTE      :   ", vote)
+product = "Apple iPhone 12 (64GB) - (Product) RED"
+variant = None
 
-print(type(reviews))
-print(len(reviews))
+if variant != None:
+    variant = variant.text
+    temp, product_name = str(variant).split("Pattern name: ")
+    temp, size = temp.split("Size: ")
+    temp, colour = temp.split("Colour: ")
+elif variant == None:
+    product_name, size, colour = product.split('(')
+    product_name = product_name.strip() + " (Generated_from_Title)"
+    size = size.split(')')[0].strip() + " (Generated_from_Title)"
+    colour = colour.split(')')[-1].strip() + " (Generated_from_Title)"
 
-#for key, value in reviews:
-#    print("Key: ", key)
-#    print("Value : ", value.encode('utf-8'))
-
-#print(response.content)
+print(product_name, " | ", size, " | ", colour)
